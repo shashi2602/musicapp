58159666da0cb9affb7ef237f0e216a42c702ed7
shashi2602 <shashi.ppk@gmail.com>
2021-06-10 18:19:29 +0530

    first commit

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
-export const HOME_SCREEN_CONTENT_URL="http://musicjiosaavnapi.herokuapp.com/home/";
-export const SEARCH="http://musicjiosaavnapi.herokuapp.com/search?query=";
-export const ALBUM_DETAILS="http://musicjiosaavnapi.herokuapp.com/album/?query=";
-export const SONG_DETAILS="http://musicjiosaavnapi.herokuapp.com/song/get/?lyrics=true&id=";
-export const PLAYLIST_DETAILS="http://musicjiosaavnapi.herokuapp.com/playlist/?query=";
+export const HOME_SCREEN_CONTENT_URL="https://musicjiosaavnapi.herokuapp.com/home/";
+export const SEARCH="https://musicjiosaavnapi.herokuapp.com/search?query=";
+export const ALBUM_DETAILS="https://musicjiosaavnapi.herokuapp.com/album/?query=";
+export const SONG_DETAILS="https://musicjiosaavnapi.herokuapp.com/song/get/?lyrics=true&id=";
+export const PLAYLIST_DETAILS="https://musicjiosaavnapi.herokuapp.com/playlist/?query=";
